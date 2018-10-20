1bd0fd26ff42085505f4d6105919e904d3e13186
StarLikeRain <pengyaokang@qq.com>
2018-10-20 17:12:13 +0800

    remove /build filter from gitignore

---
+++ build/precache-manifest.3bebed6a02428347c1f6651dcd83efed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6153004fe50fe3ae17b7",
+    "url": "/static/js/main.6153004f.chunk.js"
+  },
+  {
+    "revision": "977b3938329d8b6d2c93",
+    "url": "/static/js/1.977b3938.chunk.js"
+  },
+  {
+    "revision": "6153004fe50fe3ae17b7",
+    "url": "/static/css/main.286b029f.chunk.css"
+  },
+  {
+    "revision": "c780d641d9a04122e2ee84c85e764fc9",
+    "url": "/index.html"
+  }
+];
